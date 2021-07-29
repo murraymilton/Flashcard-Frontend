5b82af096381dca1d30890a666a4b4492f6a9f16
mmilton <murray.l.milton@outlook.com>
2021-07-29 20:20:10 +0200

    initial commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+  class App extends Component{
+
+        render(){
+          return(
+             <React.Fragment>
+               <div>
+                 <h1>Hello World</h1>
+               </div>
+             </React.Fragment>
+          )
+        }
+  }
+  export default App;
