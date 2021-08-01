af5b6833e203ef1deaf44623323aae2f11d1ca9a
mmilton <murray.l.milton@outlook.com>
2021-08-01 16:49:23 +0200

    created a custom hook form submission for handling users submission

---
+++ src/Components/formSubmit.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const useCreate = (callback) => {
+
+    const [values, setValues] = useState({});
+
+    const handleChange =(event) => {
+        event.persist();
+        setValues(values => ({...values, [event.target.name]: event.target.value}));
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        await callback();
+        setValues({})
+    }
+    return {values, handleChange, handleSubmit, setValues}
+}
+
+export default useCreate;
