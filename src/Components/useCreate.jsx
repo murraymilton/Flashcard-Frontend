488c6afc17360c4582cc7b09a9dabed88e6ace2d
mmilton <murray.l.milton@outlook.com>
2021-08-02 20:04:43 +0200

    merge conflict resloved

--- src/Components/useCreate.jsx
+++ src/Components/useCreate.jsx
@@ -23,6 +23,7 @@ export default useCreate;
 // We want to keep track of our values . Our callback function is passed into our hook and is called whenerver the form is submitted
 
 /*We’re setting up one state variable and one setter function called values and setValues.
-Then we create a function called handleSubmit which takes an event. It prevents the default action of that event (refreshing the page after the event has been called). Afterwards, it just calls callback();
-We create a function called handleChange which also takes an event.
-Finally, we return handleChange, handleSubmit and values from the custom Hook so our component has access to them. */
+-Then we create a function called handleSubmit which takes an event. 
+-It prevents the default action of that event (refreshing the page after the event has been called). Afterwards, it just calls callback();
+-We create a function called handleChange which also takes an event.
+-Finally, we return handleChange, handleSubmit and values from the custom Hook so our component has access to them. */
