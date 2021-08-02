b05f9720e7de2c61fa0cf7c1653ecbc80793bb10
mmilton <murray.l.milton@outlook.com>
2021-08-02 14:17:58 +0200

    made revisions to subcomponents

---
+++ src/Components/CollectionsList/collectionsList.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+const collectionView = (props) => {
+     return(
+         <div>
+
+         </div>
+     )
+}
+export default collectionView;
