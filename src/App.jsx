5cb5a7861aa3d918326a09e73be6756e665d7ad4
mmilton <murray.l.milton@outlook.com>
2021-08-02 07:25:57 +0200

    Added axios call for all collections to main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios';
 import "./App.css";
 
@@ -7,15 +8,27 @@ import "./App.css";
 function App(){
 
   const [collections, setCollections] = useState([]);
-  const [selected, selectedCollections] = useState([]);
-  const [selectedTitle, selectedTitle] = useState([]);
+  const [cards, selectedCards] = useState([]);
 
   useEffect(() => {
-    getAllCollections();
-  })
+    getAllCollections(); // Will return all  collections within a given set/ will add search filter 
+  }, []);
+
+  let getAllCollections = async () => {
+    try{
+      let res = await axios.get('http://127.0.0.1:8000/collections/');
+      alert(res.data)
+      setCollections(res.data)
+    }
+    catch(error) {
+      alert(error);
+    }
+  }
   return(
     <React.Fragment>
-
+      <div>
+        <h1 className="title">Flash My IQ</h1>
+      </div>
     </React.Fragment>
   )
 }
---
+++ src/Components/CardView/card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './card.css';
+
+const Card = (props) => (
+    <div className="card-container">
+        <div className="card">
+            <div className="front">
+                <div className="front back">
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+)
+export default Card
